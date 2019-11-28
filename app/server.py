2dd33fde49c2ff6e40e0f1d4cab1fbf4e91e99b0
imagixx1 <58208145+imagixx1@users.noreply.github.com>
2019-11-28 11:01:52 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1HA_nrBt7lVZeQbUrngp04Ee3PjNnp_Gz'
 export_file_name = 'fashion.pkl'
 
-classes = ['top', 'trouser','pullover','dress','coat','sandal','shirt','sneaker','bag','ankle boot']
+classes = ['polo','tshirt','jacket','suit','button','vneck','sleeveless']
 path = Path(__file__).parent
 
 app = Starlette()
